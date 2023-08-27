ee0c02d16e914ccb193b82b91e9b67b381ca38ec
Joe Aguado <aguado.joe@gmail.com>
2023-08-26 23:01:57 -0500

    random fossils added -J

--- src/random-fossil.js
+++ src/random-fossil.js
@@ -1,3 +1,32 @@
-import axios from "axios"
 
-const randomFossil
+import axios from 'axios'
+
+async function showRandomFossil(evt) {
+
+    const response = await axios.get("/random-fossil.json");
+    const fossilData = response.data
+
+document.querySelector("#random-fossil-image").innerHTML = `<img src=${fossilData.img}>`
+
+
+
+    const fossilName = document.querySelector("#random-fossil-name");
+    fossilName.textContent = fossilData.name;
+
+}
+
+document.querySelector('#get-random-fossil').addEventListener('click', showRandomFossil);
+
+
+
+// import axios from 'axios'
+
+
+
+// async function showRandomFossil(evt) {
+//     const response = await axios.get("/random-fossil.json")
+//     const fossilData = response.data.message
+//     document.querySelector("#random-fossil-image").innerHTML = `<img src=${fossilData}>`;
+//   }
+  
+//   document.querySelector('#get-random-fossil').addEventListener('click', showRandomFossil);
